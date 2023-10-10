b0c5854119acaa3de48e436333456d40dc1f9718
KingBosu <mckaywilliamiii@gmail.com>
2023-10-10 11:44:28 -0400

    Started the booking component

---
+++ app/users/components/Bookings.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Bookings = (data) => {
+  return (
+    <>
+    <div>Bookings</div>
+    <h2>Current Appointments</h2>
+    </>
+  )
+}
+
+export default Bookings
