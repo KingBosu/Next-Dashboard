428145fb7871b706a699e82c2bead39c95e268ba
KingBosu <mckaywilliamiii@gmail.com>
2023-10-12 10:31:01 -0400

    Both charts working only needs styling

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,36 +1,31 @@
 'use client'
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import LineCharts from './components/LineChart';
 import PieChart from './components/PieChart';
 import Bookings from './components/Bookings';
 import { ProfitData } from './ProfitData';
 
-interface DataItem {
-  day: string;
-  userProfits: number;
-}
-
 const Dashboard = () => {
-  const [userData, setUserData] = useState({
-    labels: 'Sunday, Monday,Tuesday, Wednesday, Thursday, Friday, Saturday',
+  const userData = {
+    labels: ProfitData.map(data => data.day),
     datasets: [{
       label: "Profits Gained",
-      data: [ProfitData.map((data) => data.userProfits)],
+      data: ProfitData.map(data => data.userProfits),
     }],
-  });
+  };
 
   return (
     <Container>
       <Bookings />
       <h1>Data Visualization</h1>
       <Row>
-        <Col md={6}>
+        <Col md={6} className="d-flex justify-content-center">
           <LineCharts chartData={userData} />
         </Col>
-        {/* <Col md={6}>
-          <PieChart data={daysOfWeekData} />
-        </Col> */}
+        <Col md={6} className="d-flex justify-content-center">
+          <PieChart data={userData} />
+        </Col>
       </Row>
     </Container>
   );
